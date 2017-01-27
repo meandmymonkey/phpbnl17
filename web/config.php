6ff9d34a8fe0d947e63a1656a9eca7cfd5cb1a2f
Andreas Hucks <andreas.hucks@sensiolabs.de>
2017-01-27 09:33:19 +0100

    added Debug component

--- web/config.php
+++ web/config.php
@@ -1,5 +1,11 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+Debug::enable(E_ALL & ~E_NOTICE);
+
 $db = new \PDO(
     'mysql:host=localhost;dbname=phpbnl17',
     'root',
